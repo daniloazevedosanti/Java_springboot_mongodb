d85688d21eef6761fbd36656790ae09a21abc5bd
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2020-06-10 21:27:58 -0300

    Created project

---
+++ src/main/java/com/ws/springbootmongodb/WsSpringbootmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.ws.springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSpringbootmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSpringbootmongodbApplication.class, args);
+	}
+
+}
