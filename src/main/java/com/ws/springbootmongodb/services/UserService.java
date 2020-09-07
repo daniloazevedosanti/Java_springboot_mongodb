040f77ea80fbcc3e702f38f7351f8b5f70a72256
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2020-09-07 17:29:45 -0300

    Obtendo um usuário por id

--- src/main/java/com/ws/springbootmongodb/services/UserService.java
+++ src/main/java/com/ws/springbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ws.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ws.springbootmongodb.domain.User;
 import com.ws.springbootmongodb.repository.UserRepository;
+import com.ws.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public Optional<User> findById(String id) {
+		Optional<User> user = repo.findById(id);
+		if (user.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado!");
+		}
+		return user;
 	}
 }
